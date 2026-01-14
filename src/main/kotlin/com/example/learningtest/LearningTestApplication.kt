0fb200f9d0ca737b52731d0451a9f6b4ea85c5bc
Yeongjae Kim <sees77770@gmail.com>
2026-01-14 20:15:52 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/learningtest/LearningTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.learningtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearningTestApplication>(*args)
+}
